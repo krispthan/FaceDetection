0f390fd5e3e74c83314b60e3ba0ccd93055adf17
krispthan <krispthan@gmail.com>
2019-12-08 18:15:01 -0800

    fixed face-detection-box

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -3,7 +3,7 @@ import "./facerecognition.scss";
 const FaceRecognition =({imageUrl, box}) => {
     return (
         <div className="center ma">
-    <div className="absolute_mt2">
+    <div className="relative mt2">
     <img id="inputImage" src={imageUrl} alt="face-recognition" width="500px" height="auto"/>
     <div className="bounding-box" style={{top:box.topRow, right:box.rightCol, bottom:box.bottomRow, left:box.leftCol}}></div>
     </div>
